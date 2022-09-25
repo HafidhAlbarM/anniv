6949d90fd410a4cb7682068ef13667fdfc86c56e
Hafidh <hafidh2704@gmail.com>
2022-09-25 08:01:31 +0700

    add music

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import { Button } from "./Button";
 import "./HeroSection.css";
 import { GiPresent } from "react-icons/gi";
+import { Howl, Howler } from "howler";
 
 const HeroSection = () => {
+  const [isPlayed, setIsPlay] = useState(false);
+
+  const soundPlay = () => {
+    const sound = new Howl({
+      src: ["/Audios/Lukas-Graham.mp3"],
+    });
+
+    sound.play();
+  };
+
+  const clickButton = () => {
+    if (!isPlayed) {
+      soundPlay();
+      setIsPlay(true);
+    }
+  };
+
+  Howler.volume(1.0);
   return (
-    <div className="hero-container">
+    <div className="hero-container" onTouchStart={() => clickButton()}>
       <video src="/Videos/ancol.mp4" autoPlay loop muted />
       <img
         src="./Images/happy-anniversary.png"
